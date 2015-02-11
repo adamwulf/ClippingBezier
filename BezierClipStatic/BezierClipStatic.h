b750f83c67016e9448eb219eec74555b773d6ae7
Adam Wulf <adam.wulf@gmail.com>
2015-02-11 17:48:23 -0600

    workign on static framework build

---
+++ BezierClipStatic/BezierClipStatic.h
@@ -0,0 +1,13 @@
+//
+//  BezierClipStatic.h
+//  BezierClipStatic
+//
+//  Created by Adam Wulf on 2/11/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BezierClipStatic : NSObject
+
+@end
---
+++ BezierClipStatic/BezierClipStatic.m
@@ -0,0 +1,13 @@
+//
+//  BezierClipStatic.m
+//  BezierClipStatic
+//
+//  Created by Adam Wulf on 2/11/15.
+//  Copyright (c) 2015 __MyCompanyName__. All rights reserved.
+//
+
+#import "BezierClipStatic.h"
+
+@implementation BezierClipStatic
+
+@end
