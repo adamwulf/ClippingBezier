f2e46815853f4e723bf304b6802a6b304c68588d
Adam Wulf <adam.wulf@gmail.com>
2020-05-08 13:40:31 -0500

    Cleaning up sample project

---
+++ ClippingExampleApp/UIBezierPath+SamplePaths.h
@@ -0,0 +1,19 @@
+//
+//  UIBezierPath+SamplePaths.h
+//  ClippingExampleApp
+//
+//  Created by Adam Wulf on 5/8/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIBezierPath (SamplePaths)
+
++ (UIBezierPath *)samplePath1;
++ (UIBezierPath *)samplePath2;
+
+@end
+
+NS_ASSUME_NONNULL_END
