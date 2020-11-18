2b2c12e379f935234557aaaca39f231b110225a1
Adam Wulf <adam.wulf@gmail.com>
2020-11-17 21:51:22 -0600

    Adding baseline test for intersection performance

--- ClippingBezierTests/MMClippingBezierPerformanceTests.m
+++ ClippingBezierTests/MMClippingBezierPerformanceTests.m
@@ -223,11 +223,8 @@
         UIBezierPath *path1 = [[self complexShape1] copy];
         UIBezierPath *path2 = [[self complexShape2] copy];
 
-        NSArray *intersections = [path1 findIntersectionsWithClosedPath:path2 andBeginsInside:NULL];
-        NSArray *shapes = [path1 allUniqueShapesWithPath:path2];
-
-        XCTAssertEqual([intersections count], 43);
-        XCTAssertEqual([shapes count], 32);
+        [path1 findIntersectionsWithClosedPath:path2 andBeginsInside:NULL];
+        [path1 allUniqueShapesWithPath:path2];
     }];
 }
 
