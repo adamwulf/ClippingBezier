1db3f912cf531031bd0a03fb2942e6e6db92bd77
Adam Wulf <adam.wulf@gmail.com>
2021-04-04 23:13:44 -0500

    Adding PerformanceBezier to package file

--- Package.swift
+++ Package.swift
@@ -20,7 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ClippingBezier",
-            dependencies: [],
+            dependencies: [
+                .package(url: "git@github.com:adamwulf/PerformanceBezier.git", from: "1.2.0"),
+            ],
             path: "ClippingBezier",
             exclude: ["Info.plist", "ClippingBezier-Info.plist"],
             publicHeadersPath: "."),
