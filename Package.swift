19b59ba58158f8e2c05030a14b5bf570a977e093
Adam Wulf <adam.wulf@gmail.com>
2021-08-03 17:47:30 -0500

    Bump to 1.2.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/adamwulf/PerformanceBezier.git", from: "1.2.0"),
+        .package(url: "https://github.com/adamwulf/PerformanceBezier.git", from: "1.3.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
