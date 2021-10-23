5881fb60a40e5230b55dc8e023927647d0cd63ac
Adam Wulf <adam.wulf@gmail.com>
2021-10-22 19:17:43 -0500

    Removing cocoapods support for muse

--- ClippingBezier/PublicHeaders/ClippingBezier.h
+++ ClippingBezier/PublicHeaders/ClippingBezier.h
@@ -8,30 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-// SwiftPackageManager needs imports to be "", while frameworks generally want <>
-// It's unfortunately not enough to `#if SWIFT_PACKAGE`, as if ClippingBezier
-// is a dependency of another SPM package, then the SWIFT_PACKAGE seems to not be
-// set when building that dependency. So instead, we'll choose based off of Cocoapods
-
-#if COCOAPODS
-#import <ClippingBezier/DKIntersectionOfPaths.h>
-#import <ClippingBezier/DKIntersectionOfPaths.h>
-#import <ClippingBezier/DKUIBezierPathShape.h>
-#import <ClippingBezier/DKUIBezierPathClippedSegment.h>
-#import <ClippingBezier/UIBezierPath+Clipping.h>
-#import <ClippingBezier/UIBezierPath+Intersections.h>
-#import <ClippingBezier/UIBezierPath+GeometryExtras.h>
-#import <ClippingBezier/UIBezierPath+DKOSX.h>
-#import <ClippingBezier/UIBezierPath+Trimming.h>
-#import <ClippingBezier/DKUIBezierPathClippedSegment.h>
-#import <ClippingBezier/DKUIBezierPathClippingResult.h>
-#import <ClippingBezier/DKUIBezierPathIntersectionPoint.h>
-#import <ClippingBezier/DKUIBezierUnmatchedPathIntersectionPoint.h>
-#import <ClippingBezier/DKUIBezierPathShape.h>
-#import <ClippingBezier/DKTangentAtPoint.h>
-#import <ClippingBezier/DKVector.h>
-#import <ClippingBezier/MMBackwardCompatible.h>
-#else
 #import "DKIntersectionOfPaths.h"
 #import "DKIntersectionOfPaths.h"
 #import "DKUIBezierPathShape.h"
@@ -49,5 +25,4 @@
 #import "DKTangentAtPoint.h"
 #import "DKVector.h"
 #import "MMBackwardCompatible.h"
-#endif
 //#include "bezierclip.hxx"
