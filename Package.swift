3613e8d11a8653d4d1636205b5ed483fbea36d95
Adam Wulf <adam@adamwulf.me>
2026-05-01 17:52:43 -0500

    Update PerformanceBezier to main branch at 2b15a68

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/adamwulf/PerformanceBezier.git", .branch("muse")),
+        .package(url: "https://github.com/adamwulf/PerformanceBezier.git", .branch("main")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
